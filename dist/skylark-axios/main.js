eeca1fd398208bb6cad6b7d2fc62fada6c376216
hudaokeji <github@hudaokeji.com>
2020-01-31 13:55:37 +0900

    init commit

---
+++ dist/skylark-axios/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-axios - A version of axios.js that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-axios/
+ * @license MIT
+ */
+define(["./axios"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-axios/main.js
@@ -0,0 +1,5 @@
+define([
+	"./axios"
+],function(axios){
+	return axios;
+});
